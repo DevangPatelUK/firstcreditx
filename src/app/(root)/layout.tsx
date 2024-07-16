d5162ccc3cdc37b48887601007cc33558ca1070c
Devang <devang@Devangs-MacBook-Pro.local>
2024-07-16 18:14:06 +0530

    Stage 1

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default RootLayout;
 
@@ -13,8 +15,19 @@ function RootLayout({
    <main className="flex h-screen w-full font-inter">
 
     <Sidebar user={loggedIn} />
+    <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" width={30} height={30}
+             alt="logo"
+            />
+            <div>
+              <MobileNav user={loggedIn}/>
+            </div>
+          </div>     
+          {children}
 
-    {children}
+    </div>
+
    </main>
   );
 }
